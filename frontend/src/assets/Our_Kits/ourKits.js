4722a2bd4fec5cad0d78af98766c1d1c3bcc6e36
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-12-22 13:43:57 +0530

    fix

--- frontend/src/assets/Our_Kits/ourKits.js
+++ frontend/src/assets/Our_Kits/ourKits.js
@@ -11,7 +11,7 @@ import oled_display from '../Our_Kits/oled-display.jpeg'
 import jumper_wire from '../Our_Kits/jumper_wire.jpeg'
 import c_type_usb_cable from '../Our_Kits/c-type-cable.jpg'
 import screw_driver from '../Our_Kits/screw-driver.jpeg'
-import li_ion_battery from '../Our_Kits/li-ion-battery.jpeg'
+import li_ion_battery from '../Our_Kits/Li-ion-battery.jpeg'
 import screws from '../Our_Kits/screws.jpeg'
 import dc_motor from '../Our_Kits/dc-moters.jpeg'
 import moter_clip from '../Our_Kits/moter-clip.jpeg'
@@ -19,6 +19,7 @@ import wheels from '../Our_Kits/wheels.jpeg'
 import leelaboard from '../Our_Kits/leela-board.jpeg'
 import ir_sensor from '../Our_Kits/irSensor.jpeg'
 import pir_sensor from '../Our_Kits/pr-sensor.webp'
+
 
 export const ourKits = {
     yellow_kit,
